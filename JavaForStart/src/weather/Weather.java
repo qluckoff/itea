4dff1f3e2aa486a5c37dc445a60277a24104993c
Kiril Kulakov <kiril.kulakov@corp.kismia.com>
2022-11-02 16:50:11 +0200

    added weather

--- JavaForStart/src/weather/Weather.java
+++ JavaForStart/src/weather/Weather.java
@@ -2,4 +2,9 @@ package weather;
 
 public class Weather {
 
+    public static void main(String[] args) {
+
+        System.out.println("    Погода в Барселоні    \nCьогодні: +23, Сонячно\nЗавтра: +21, Мінлива хмарність");
+    }
+
 }

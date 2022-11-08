1a2bc931f1f06714d24faef09fb8f19cf9ca4682
Kiril Kulakov <kiril.kulakov@corp.kismia.com>
2022-11-08 20:38:04 +0200

    added hw3

--- JavaForStart/src/homework/HW1.java
+++ JavaForStart/src/homework/HW1.java
@@ -1,3 +1,5 @@
+package homework;
+
 public class HW1 {
 
     int a = 10;
---
+++ JavaForStart/src/homework/HW2.java
@@ -0,0 +1,16 @@
+package homework;
+
+public class HW2 {
+
+    public static void main(String[] args) {
+        int number = 3;
+        char subTask;
+        int x = 13;
+        int y = 23;
+        int z = 31;
+
+        number++;
+        System.out.println("Task " + number);
+
+    }
+}
